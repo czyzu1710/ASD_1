61c39edb3ae637294c4ff3024b529215f954754b
czyzu1710 <czyzu1710@gmail.com>
2018-10-25 10:46:32 +0200

    second commit

--- include/cursorlist.hpp
+++ include/cursorlist.hpp
@@ -1,38 +1,45 @@
-#ifndef CURSOR_LIST_H_
-#define CURSOR_LIST_H_
+// Copyright 2018 <Michał Czyż>
+#ifndef INCLUDE_CURSORLIST_HPP_
+#define INCLUDE_CURSORLIST_HPP_
 #include <iostream>
 using namespace std;
-using position_t=int;
+using position_t = int;
 template<typename T>
-class CursorList
-{
+class CursorList{
   CursorList<T>();
   CursorList<T>(int max_num);
-  void push_front( T element);
+  void push_front(T element);
   void push_back(T element);
   T pop_front();
   T pop_back();
   position_t find(T element);
   position_t erase(position_t pos);
-  position_t insert(position_t pos,T element);
+  position_t insert(position_t pos, T element);
   int size();
-  bool empty(){
-    if(actual_size==0)return true;
-    else return false;
+  bool empty() {
+    if (actual_size == 0) return true;
+    else
+    return false;
   }
   class Node;
-private:
+ private:
   Node *tab;
   int head;
   int free;
   int actual_size;
-
 };
+
 template <typename T>
-class CursorList <T> ::Node
-{
-public:
+class CursorList <T> ::Node{
+ public:
   T value;
   T next;
 };
-#endif
+
+template<typename T>
+CursorList<T>::CursorList():head{0}, free{0}, actual_size{0} {
+tab = new Node[1000000];
+}
+
+
+#endif  // INCLUDE_CURSORLIST_HPP_
